776a1994297682158af629adbbbf5f645c258ab1
Till Hellmund <thellmund@gmail.com>
2018-11-02 17:09:57 +0100

    Remove unused code in Action

--- library/src/main/java/com/hellmund/library/actions/Action.kt
+++ library/src/main/java/com/hellmund/library/actions/Action.kt
@@ -18,13 +18,6 @@ abstract class Action(
     val tintColor: Int? = null
 ) {
 
-    /**
-     * Returns a copy of the original object and overrides the properties that are provided as arguments. This is a
-     * custom implementation, as only data classes come with a copy() method and data classes can't be used due to the
-     * need for inheritance.
-     */
-    abstract fun copy(labelResource: LabelResource? = null, iconResource: IconResource? = null, tintColor: Int? = null): Action
-
     open fun onListItemInflated(itemView: View) {
         // Free ad space
     }
